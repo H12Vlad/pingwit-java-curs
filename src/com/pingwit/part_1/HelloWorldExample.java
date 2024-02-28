0bf3fa59afb0d5db895de224e3ac51ac6b29ee1f
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-02-28 20:37:57 +0100

    12 commit

--- src/com/pingwit/part_1/HelloWorldExample.java
+++ src/com/pingwit/part_1/HelloWorldExample.java
@@ -1,4 +1,4 @@
-package com.pingwit.parts_0;
+package com.pingwit.part_1;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
