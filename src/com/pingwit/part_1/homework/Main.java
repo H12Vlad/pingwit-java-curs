299ff708130b9deb31521915a6a3915ad567120f
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-02-29 20:53:06 +0100

    Initial commit

--- src/com/pingwit/part_1/homework/Main.java
+++ src/com/pingwit/part_1/homework/Main.java
@@ -4,37 +4,34 @@ package com.pingwit.part_1.homework;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("                                 Part_1 : "); // не надо такие отступы делать внутри ковычек, если очень хочется можешь \t добавить перед Part_1
+        System.out.println("\tPart_1 : "); // не надо такие отступы делать внутри ковычек, если очень хочется можешь \t добавить перед Part_1
         System.out.println("Task 1, 4 : ");
-        System.out.println("       Hello, World!"); // не надо такие отступы делать внутри ковычек
+        System.out.println("Hello, World!"); // не надо такие отступы делать внутри ковычек
 
         System.out.println("Task 5 : ");
         int k = 10;
         int t = 22;
         int sum = k + t;
-        System.out.println("      Сума двух чисел : " + sum); // не надо такие отступы делать внутри ковычек
+        System.out.println("Сума двух чисел : " + sum); // не надо такие отступы делать внутри ковычек
 
 
         System.out.println("Task 6 :");
         int i = 2;
         int m = 22;
         int result = i * m;
-        System.out.println("        Произведение двух чисел : " + result); // не надо такие отступы делать внутри ковычек
+        System.out.println("Произведение двух чисел : " + result); // не надо такие отступы делать внутри ковычек
 
 
         System.out.println("Task 7 :");
         int x = 100;
         int y = 10;
-        int result2 = x % y;
-        System.out.println("         Остаток от деления двух чисел : " + result); // не надо такие отступы делать внутри ковычек
+        int sum2 = x % y;
+        System.out.println("Остаток от деления двух чисел : " + sum2); // не надо такие отступы делать внутри ковычек
 
         System.out.println("Task 8 : ");
-        String name = " Vladyskav ";
+        String name = "Vladyslav";
         String name2 = "Hoidra";
-        System.out.println("         Сложение двух строк : ");
-        System.out.println("          " + name + " " + name2);
-//это лишняя строка
-//это лишняя строка
+        System.out.println("Сложение двух строк : ");
+        System.out.println(" " + name + " " + name2);
     }
-//это лишняя строка
 }
--- src/com/pingwit/part_3/homework/CurrencyRates.java
+++ src/com/pingwit/part_3/homework/CurrencyRates.java
@@ -1,24 +1,33 @@
-package com.homeworks_part3;
+
+package com.pingwit.part_3.homework;
 
 // отформатируй код
 public class CurrencyRates {
     public static void main(String[] args) {
-        int dollars = 22;
-        int currencyId = 1;
+        int zloty = 10;
+        int currencyId = 2;
 
-        double convertedDollars;
-        switch (currencyId){
+        double convertedZloty;
+        switch (currencyId) {
             case 1: // как насчет добавить больше 1 кейса?
-                convertedDollars = dollars * 4.28;
-                System.out.println("Currency to Dollarss: " + convertedDollars); // опечатка в слове Dollarss
+                convertedZloty = zloty * 4.00;
+                System.out.println("Currency to Dollars: " + convertedZloty); // опечатка в слове Dollarss
                 break;
+            case 2: // как насчет добавить больше 1 кейса?
+                convertedZloty = zloty * 0.048;
+                System.out.println("Currency to Indian rupees: " + convertedZloty);
+                break;
+            case 3: // как насчет добавить больше 1 кейса?
+                convertedZloty = zloty * 4.32;
+                System.out.println("Currency to Euro: " + convertedZloty);
+                break;
+            case 4: // как насчет добавить больше 1 кейса?
+                convertedZloty = zloty * 0.13;
+                System.out.println("Currency to Egyptian pound: " + convertedZloty);
+                break;
+
             default:
-                System.out.println("Problem identifical ID"); // думаю над текстом ошибки еще стоит поработать
-//это лишняя строка
-//это лишняя строка
-//это лишняя строка
-            }
-//это лишняя строка
+                System.out.println("Identification ID issue: "); // думаю над текстом ошибки еще стоит поработать
         }
-//это лишняя строка
     }
+}
