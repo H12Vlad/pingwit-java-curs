ab92f1a8063b8ca754f6ba55c44d12c31f25f303
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-03-03 18:15:45 +0100

    Commit 3

--- src/com/pingwit/part_3/homework/CurrencyRates.java
+++ src/com/pingwit/part_3/homework/CurrencyRates.java
@@ -1,33 +1,29 @@
 
 package com.pingwit.part_3.homework;
-
-// отформатируй код
 public class CurrencyRates {
     public static void main(String[] args) {
         int zloty = 10;
         int currencyId = 2;
-
         double convertedZloty;
         switch (currencyId) {
-            case 1: // как насчет добавить больше 1 кейса?
+            case 1:
                 convertedZloty = zloty * 4.00;
-                System.out.println("Currency to Dollars: " + convertedZloty); // опечатка в слове Dollarss
+                System.out.println("Currency to Dollars: " + convertedZloty);
                 break;
-            case 2: // как насчет добавить больше 1 кейса?
+            case 2:
                 convertedZloty = zloty * 0.048;
                 System.out.println("Currency to Indian rupees: " + convertedZloty);
                 break;
-            case 3: // как насчет добавить больше 1 кейса?
+            case 3:
                 convertedZloty = zloty * 4.32;
                 System.out.println("Currency to Euro: " + convertedZloty);
                 break;
-            case 4: // как насчет добавить больше 1 кейса?
+            case 4:
                 convertedZloty = zloty * 0.13;
                 System.out.println("Currency to Egyptian pound: " + convertedZloty);
                 break;
-
-            default:
-                System.out.println("Identification ID issue: "); // думаю над текстом ошибки еще стоит поработать
+                default:
+                System.out.println("Identification ID issue: ");
         }
     }
 }
