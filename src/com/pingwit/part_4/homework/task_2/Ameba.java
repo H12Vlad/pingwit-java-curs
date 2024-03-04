db5983ecbf308798e5b3379aab05566b088db3e2
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 17:14:12 +0100

    Update Ameba.java

--- src/com/pingwit/part_4/homework/task_2/Ameba.java
+++ src/com/pingwit/part_4/homework/task_2/Ameba.java
@@ -1,9 +1,9 @@
 package com.pingwit.part_4.homework.task_2;
 
-public class Ameba {
+public class Ameba { // Ameba -> Amoeba, у тебя опечатка в названии класса
     public static void main(String[] args) {
-        int a = 1;
-        for (int h = 3; h <= 24; h += 3) {
+        int a = 1; // придумай для 'a' понятное название переменной
+        for (int h = 3; h <= 24; h += 3) { // h -> hour
             a *= 2;
             System.out.println("Через " + h + " часа(ов) будет " + a + " амёб(ы)");
         }
