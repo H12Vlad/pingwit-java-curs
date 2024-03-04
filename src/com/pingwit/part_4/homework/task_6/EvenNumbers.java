722a5819c1146dc2d30174824524ffe39a526f07
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 17:23:08 +0100

    Update EvenNumbers.java

--- src/com/pingwit/part_4/homework/task_6/EvenNumbers.java
+++ src/com/pingwit/part_4/homework/task_6/EvenNumbers.java
@@ -2,7 +2,7 @@ package com.pingwit.part_4.homework.task_6;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        System.out.println("Четные числа от 2 до 100:");
+        System.out.println("Четные числа от 2 до 100:"); // Ты скорее всего знаешь что такое конкатенация строк, значит, можно 2 и 100 вынести в отдельную переменную и менять значение в одном месте.
         for (int i = 2; i <= 100; i += 2) {
             System.out.println(i);
         }
