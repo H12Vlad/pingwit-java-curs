1c2844ab6198c58f63e33472ad7f56ddcbd95bb0
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 17:15:28 +0100

    Update Calculation.java

--- src/com/pingwit/part_4/homework/task_3/Calculation.java
+++ src/com/pingwit/part_4/homework/task_3/Calculation.java
@@ -2,8 +2,8 @@ package com.pingwit.part_4.homework.task_3;
 
 public class Calculation {
     public static void main(String[] args) {
-        int c = 1;
-        for (int i = 1; i <= 256; i *= 2) {
+        int c = 1; // придумай для 'c' понятное название переменной 
+        for (int i = 1; i <= 256; i *= 2) { // для 'i' название можно не менять, т.к. это стандартное название для счетчика цикла
             c += i;
         }
         System.out.println("Сумма: " + c);
