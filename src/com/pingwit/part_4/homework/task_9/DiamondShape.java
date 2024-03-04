4edb35d83b83b54e0924d85a82709ba793bffd91
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 17:28:41 +0100

    Update DiamondShape.java

--- src/com/pingwit/part_4/homework/task_9/DiamondShape.java
+++ src/com/pingwit/part_4/homework/task_9/DiamondShape.java
@@ -2,18 +2,18 @@ package com.pingwit.part_4.homework.task_9;
 
 public class DiamondShape {
     public static void main(String[] args) {
-        int size = 5;
+        int size = 5; // опционально можно добавить абзац
         for (int i = 1; i <= size; i++) {
-            for (int j = size - i; j > 0; j--) {
+            for (int j = size - i; j > 0; j--) { // этот цикл и следующий давай объеденим в один
                 System.out.print(" ");
             }
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
-        }
+        } // вот сюда добавь абзац, чтобы циклы не сливались
         for (int i = size - 1; i >= 1; i--) {
-            for (int j = size - i; j > 0; j--) {
+            for (int j = size - i; j > 0; j--) { // этот цикл и следующий давай объеденим в один
                 System.out.print(" ");
             }
             for (int j = 1; j <= 2 * i - 1; j++) {
