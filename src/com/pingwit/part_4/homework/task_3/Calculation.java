f6be7e4f847cf75eca93f81c77aef4581cca237f
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-03-09 21:07:09 +0100

    Commit part4, patr5 homework1

--- src/com/pingwit/part_4/homework/task_3/Calculation.java
+++ src/com/pingwit/part_4/homework/task_3/Calculation.java
@@ -3,7 +3,7 @@ package com.pingwit.part_4.homework.task_3;
 public class Calculation {
     public static void main(String[] args) {
         int calculator = 1;
-        for (int i = 1; i <= 256; i *= 2) { // для 'i' название можно не менять, т.к. это стандартное название для счетчика цикла
+        for (int i = 1; i <= 256; i *= 2) {
             calculator += i;
         }
         System.out.println("Сумма: " + calculator);
