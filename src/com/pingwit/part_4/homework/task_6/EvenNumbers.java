acd9c0c8e96ada6ded6d068f5fb564c3d8c8fb86
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-10 13:49:06 +0100

    Update EvenNumbers.java

--- src/com/pingwit/part_4/homework/task_6/EvenNumbers.java
+++ src/com/pingwit/part_4/homework/task_6/EvenNumbers.java
@@ -2,7 +2,7 @@ package com.pingwit.part_4.homework.task_6;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        String name = "от 2";
+        String name = "от 2"; // в переменных name и name2 нету необходимости
         String name2 = "до 100";
         System.out.println(" " + name + " " + name2);
         for (int i = 2; i <= 100; i += 2) {
