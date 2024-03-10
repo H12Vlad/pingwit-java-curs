613f972a0373742d6835f60f1d9c457965f8e032
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-10 13:43:49 +0100

    Update Amoeba.java

--- src/com/pingwit/part_4/homework/task_2/Amoeba.java
+++ src/com/pingwit/part_4/homework/task_2/Amoeba.java
@@ -5,7 +5,7 @@ public class Amoeba {
         int amoeba = 1;
         for (int hour = 3; hour <= 24; hour += 3) {
             amoeba *= 2;
-            System.out.println("Через " + hour + " часа(ов) будет " + amoeba + " амёб(ы))");
+            System.out.println("Через " + hour + " часа(ов) будет " + amoeba + " амёб(ы))"); // (ы)) - одна лишняя скобка
         }
     }
 }
