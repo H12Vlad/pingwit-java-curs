2e28765e3437484930a5b7b09c67aeb8baf1ef82
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-04-14 11:16:15 +0200

    xxx

---
+++ src/com/pingwit/part_9/homework/task_1/ImmutableExample.java
@@ -0,0 +1,19 @@
+package com.pingwit.part_9.homework.task_1;
+
+
+import java.util.Arrays;
+
+public class ImmutableExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        Warehouse citadel = new Warehouse(numbers);
+
+//        citadel.next();
+        System.out.println(citadel);
+        System.out.println("Next element: " + citadel.next());
+        System.out.println("Next element: " + citadel.next());
+        System.out.println("Next element: " + citadel.next());
+        System.out.println("Array: " + Arrays.toString(citadel.getArray()));
+
+    }
+}
---
+++ src/com/pingwit/part_9/homework/task_1/Warehouse.java
@@ -0,0 +1,53 @@
+package com.pingwit.part_9.homework.task_1;
+
+import java.util.Arrays;
+/*
+Я хз что делать с методом next ()
+Я даже не имею представление как должно выглядеть вывод на экран который написан
+" В классе должен быть метод next() который возвращает следующий элемент массива."
+(Возможно я знаю но скорее всего я запутался)
+ */
+
+public class Warehouse {
+    int[] numbers;
+    int index;
+
+        public Warehouse(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+
+//    public int next() {
+//        if (index == numbers.length - 1) {
+//            index = 0;
+//        } else {
+//            index++;
+//        }
+//        return numbers[index];
+//
+//    }
+        public int next () {
+            index = (index + 1) % numbers.length;
+            return numbers[index];
+        }
+
+        public int[] numbers () {
+            int[] clone = new int[numbers.length];
+            for (int i = 0; i < clone.length; i++) {
+                clone[i] = numbers[i];
+            }
+            return clone;
+
+        }
+        public int[] getArray () {
+            return Arrays.copyOf(numbers, numbers.length);
+        }
+
+        @Override
+        public String toString () {
+            return "Warehouse{" +
+                    "numbers=" + Arrays.toString(numbers) +
+                    ", index=" + index +
+                    '}';
+        }
+    }
