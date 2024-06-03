d1bf612b1c85acce06ef754360e5e01169c4e11d
Alexander Fomin <rezviybelorus@gmail.com>
2024-06-03 22:38:38 +0200

    check homework 12,13

--- src/com/pingwit/part_13/homework/task_1/ZigzagRoad.java
+++ src/com/pingwit/part_13/homework/task_1/ZigzagRoad.java
@@ -8,19 +8,19 @@ public class ZigzagRoad {
         int counter = 100;
         Boolean increment = false;
         int right = 10;
-        int left = 0;
+        int left = 0; // попробуй сделать так, чтобы переменная left контролировала расстояние дороги от левого края, т.е. если left = 10, то дорога печатается ближе к центру экрана
         int smallerNumber = 0;
-        while (counter > smallerNumber) {
+        while (counter > smallerNumber) { // в данном случае можно оставить while (counter > 0)
             for (int i = 0; i < tab; i++) {
                 System.out.print(emptyString);
-            }
+            } // для лучшей читаемости здесь можно добавить абзац
             System.out.println(road);
             counter--;
             if (increment) {
                 tab++;
             } else {
                 tab--;
-            }
+            }// для лучшей читаемости здесь можно добавить абзац
             if (tab == left) {
                 increment = true;
             }
