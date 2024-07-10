58b8298fa432801307dc8734bb9898ebc6312d15
Alexander Fomin <rezviybelorus@gmail.com>
2024-07-10 09:28:18 +0200

    check homework 18

--- src/com/pingwit/part_18/homework/task_5/CopyingFileBober.java
+++ src/com/pingwit/part_18/homework/task_5/CopyingFileBober.java
@@ -8,14 +8,15 @@ import java.io.IOException;
 public class CopyingFileBober {
     public static void main(String[] args) {
         String sourcePath = "src/com/pingwit/part_18/homework/task_5/Bober.png";
-        String destinationPath = "src/com/pingwit/part_18/homework/task_5/";
+        String destinationPath = "src/com/pingwit/part_18/homework/task_5/"; // destinationPath = new File(sourcePath).getParent
 
         File sourceFile = new File(sourcePath);
         String fileName = sourceFile.getName();
 
-        int dotIndex = fileName.lastIndexOf('.');
-        String nameWithoutExtension = (dotIndex == -1) ? fileName : fileName.substring(0, dotIndex);
-        String extension = (dotIndex == -1) ? "" : fileName.substring(dotIndex);
+        int dotIndex = fileName.lastIndexOf('.'); // dotIndex -> lastDotIndex / extensionIndex
+        // boolean hasExtension = dotIndex == -1;
+        String nameWithoutExtension = (dotIndex == -1) ? fileName : fileName.substring(0, dotIndex); // dotIndex == -1 -> hasExtension
+        String extension = (dotIndex == -1) ? "" : fileName.substring(dotIndex); // dotIndex == -1 -> hasExtension
 
         String copyName = nameWithoutExtension + "_copy" + extension;
 
@@ -29,8 +30,8 @@ public class CopyingFileBober {
 
         try (FileInputStream fis = new FileInputStream(sourcePath);
              FileOutputStream fos = new FileOutputStream(destinationFile)
-        ) {
-
+        ) { // в джава ) { обычно оставляют в 32 строке
+// лишняя строка, удали
             byte[] buffer = new byte[1024];
             int length;
             while ((length = fis.read(buffer)) > 0) {
