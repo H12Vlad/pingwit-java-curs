fa372316b0268102a158a03f4ce47f380ea1f9a9
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-07-14 13:30:33 +0200

    check homework 18 again

--- src/com/pingwit/part_18/homework/task_5/CopyingFileBober.java
+++ src/com/pingwit/part_18/homework/task_5/CopyingFileBober.java
@@ -8,30 +8,10 @@ import java.io.IOException;
 public class CopyingFileBober {
     public static void main(String[] args) {
         String sourcePath = "src/com/pingwit/part_18/homework/task_5/Bober.png";
-        String destinationPath = "src/com/pingwit/part_18/homework/task_5/"; // destinationPath = new File(sourcePath).getParent
-
-        File sourceFile = new File(sourcePath);
-        String fileName = sourceFile.getName();
-
-        int dotIndex = fileName.lastIndexOf('.'); // dotIndex -> lastDotIndex / extensionIndex
-        // boolean hasExtension = dotIndex == -1;
-        String nameWithoutExtension = (dotIndex == -1) ? fileName : fileName.substring(0, dotIndex); // dotIndex == -1 -> hasExtension
-        String extension = (dotIndex == -1) ? "" : fileName.substring(dotIndex); // dotIndex == -1 -> hasExtension
-
-        String copyName = nameWithoutExtension + "_copy" + extension;
-
-        File destinationFile = new File(destinationPath + copyName);
-        int copyIndex = 1;
-        while (destinationFile.exists()) {
-            copyName = nameWithoutExtension + "_copy(" + copyIndex + ")" + extension;
-            destinationFile = new File(destinationPath + copyName);
-            copyIndex++;
-        }
+        File destinationFile = getFile(sourcePath);
 
         try (FileInputStream fis = new FileInputStream(sourcePath);
-             FileOutputStream fos = new FileOutputStream(destinationFile)
-        ) { // в джава ) { обычно оставляют в 32 строке
-// лишняя строка, удали
+             FileOutputStream fos = new FileOutputStream(destinationFile)) {
             byte[] buffer = new byte[1024];
             int length;
             while ((length = fis.read(buffer)) > 0) {
@@ -43,5 +23,28 @@ public class CopyingFileBober {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static File getFile(String sourcePath) {
+        String destinationPath = "src/com/pingwit/part_18/homework/task_5/";
+
+        File sourceFile = new File(sourcePath);
+        String fileName = sourceFile.getName();
+
+        int lastDotIndex = fileName.lastIndexOf('.');
+        boolean hasExtension = lastDotIndex == -1;
+        String nameWithoutExtension = (hasExtension) ? fileName : fileName.substring(0, lastDotIndex);
+        String extension = (hasExtension) ? "" : fileName.substring(lastDotIndex);
+
+        String copyName = nameWithoutExtension + "_copy" + extension;
+
+        File destinationFile = new File(destinationPath + copyName);
+        int copyIndex = 1;
+        while (destinationFile.exists()) {
+            copyName = nameWithoutExtension + "_copy(" + copyIndex + ")" + extension;
+            destinationFile = new File(destinationPath + copyName);
+            copyIndex++;
+        }
+        return destinationFile;
     }
 }
