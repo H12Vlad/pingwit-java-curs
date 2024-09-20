f3e08996192670e5b3117125adb2567348c0540f
Alexander Fomin <rezviybelorus@gmail.com>
2024-09-20 21:11:42 +0200

    [Vladyslav Hoidra] check homework 25

--- src/com/pingwit/part_25/homework/task_4/Car.java
+++ src/com/pingwit/part_25/homework/task_4/Car.java
@@ -1,4 +1,4 @@
 package com.pingwit.part_25.homework.task_4;
-// отформатируй код
+// отформатируй код, все еще отформартируй код
 public record Car(String model,String manufacturer,Integer age,String engineType) { // age -> year будет правильнее
 }
