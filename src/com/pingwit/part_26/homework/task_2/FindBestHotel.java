453f9c982edd6d2b523df849bc860eb3e996611c
Vladyslav Hoidra <160567718+H12Vlad@users.noreply.github.com>
2024-09-21 23:47:34 +0200

    Homework_26 task_2

--- src/com/pingwit/part_26/homework/task_2/FindBestHotel.java
+++ src/com/pingwit/part_26/homework/task_2/FindBestHotel.java
@@ -1,10 +1,11 @@
 package com.pingwit.part_26.homework.task_2;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class FindBestHotel {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<Hotel> hotels = List.of(
                 new Hotel("Diamond Hotel", 4.5),
                 new Hotel("Seaside Resort", 4.7),
@@ -13,9 +14,8 @@ public class FindBestHotel {
                 new Hotel("Country House", 4.6)
         );
 
-        Optional<Hotel> bestHotel = Optional.ofNullable(hotels.stream() // А для чего ты здесь используешь Optional.ofNullable()?
-                .reduce((h1, h2) -> h1.rating() >= h2.rating() ? h1 : h2) // метод reduce немного для других целей, здесь лучше использовать map() -> max()
-                .orElseThrow(() -> new RuntimeException("Список отелей пуст!!!")));
+        Optional<Hotel> bestHotel = hotels.stream()
+                .max(Comparator.comparing(Hotel::rating));
 
         System.out.println("!!!Лучший отель!!!: " + bestHotel);
     }
